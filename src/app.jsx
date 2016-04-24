4161a52ce44f91a4b26603d410c2507e9c5b8f86
takatori <tktr3w5e0@gmail.com>
2016-04-24 21:45:49 +0900

    First commit

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import * as actions from './actions'
+import App from './components/App'
+
+const store = createStore(todoApp)
+
+render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('container')
+)
+
+
---
+++ src/reducers/todos.js
@@ -0,0 +1,17 @@
+// Reducers
+function todos(state = [], action) {
+    switch(action.type) {
+    case 'ADD_TODO':
+        return [
+                ...state,
+            { text: action.text, completed: false }
+        ]
+    case 'COMPMLETE_TODO':
+        // 要実装
+        return state
+    default:
+        return state
+    }
+}
+
+export default todos
